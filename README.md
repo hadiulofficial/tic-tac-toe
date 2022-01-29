f2dedd5c5098683356cbcdb277d8f97c8b898a5b
hadiul-islam <hadiulislamofficial@gmail.com>
2022-01-29 17:57:54 +0600

    created board and square component

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tic Tac Toe
+
+
+ > Tic Tac Toe is a game created by `reactjs`
+
+**To run this project**,
+  * First clone this repo
+  * Run `yarn install` or `npm install`
+  * Run `yarn start` or `npm start`
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './Components/Board';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
