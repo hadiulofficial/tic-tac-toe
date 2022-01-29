71004f3ca873155281c2fde9fa1de0c821740684
hadiul-islam <hadiulislamofficial@gmail.com>
2022-01-29 21:01:07 +0600

    added board functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Board from './Components/Board';
 
+import './Styles/global.scss';
+
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>Tic Tac Toe</h1>
       <Board />
     </div>
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Square = ({value}) => {
-    return <button>{value}</button>
+const Square = ({ value, onClick }) => {
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    )
 };
 
 export default Square;
